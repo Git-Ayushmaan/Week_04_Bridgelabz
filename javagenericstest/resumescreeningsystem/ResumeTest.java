a10a3e8c61a54d56e02f8456f10bd92dce53f784
Git-Ayushmaan <20ayushmaan@gmail.com>
2025-02-13 10:44:59 +0530

    [Ayushmaan Tiwari] Add.  AI-Driven Resume Screening System Test Added

---
+++ javagenericstest/resumescreeningsystem/ResumeTest.java
@@ -0,0 +1,30 @@
+package com.example.javagenerics.resumescreeningsystem;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import java.util.List;
+
+public class ResumeTest extends TestCase {
+    @Test
+    public void testDataScientist(){
+        Resume<DataScientist> dataScientistResume=new Resume<>();
+        dataScientistResume.add(new DataScientist("Sweta Agrawal","Data Scientist"));
+        List<DataScientist> items=dataScientistResume.getList();
+        assertEquals(1,items.size());
+    }
+    @Test
+    public void testSoftwareEngineer(){
+        Resume<SoftwareEngineer> softwareEngineerResume=new Resume<>();
+        softwareEngineerResume.add(new SoftwareEngineer("Sweta Agrawal","Data Scientist"));
+        List<SoftwareEngineer> items=softwareEngineerResume.getList();
+        assertEquals(1,items.size());
+    }
+    @Test
+    public void testProductManager(){
+        Resume<ProductManager> productManagerResume=new Resume<>();
+        productManagerResume.add(new ProductManager("Sweta Agrawal","Data Scientist"));
+        List<ProductManager> items=productManagerResume.getList();
+        assertEquals(1,items.size());
+    }
+}
