fffb342d90840677281fc735bd4267c09665fca0
Git-Ayushmaan <20ayushmaan@gmail.com>
2025-02-14 12:27:03 +0530

    [Ayushmaan Tiwari] Add. counting frequency of 5 most frequent words problem added

---
+++ javastreams/countwordfrequency/Main.java
@@ -0,0 +1,7 @@
+package com.example.javastreams.countwordfrequency;
+public class Main {
+    public static void main(String[] args) {
+        String fileName = "D:\\Capgemini\\source.txt";
+        WordFrequencyCounter.wordFrequencyCounter(fileName);
+    }
+}
