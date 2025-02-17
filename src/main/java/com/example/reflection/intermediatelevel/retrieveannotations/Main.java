aab74a505d9663582794bda30177f5f5c4414950
Git-Ayushmaan <20ayushmaan@gmail.com>
2025-02-17 14:55:03 +0530

    [Ayushmaan Tiwari]Add. Reflection intermediate level problems files added

---
+++ src/main/java/com/example/reflection/intermediatelevel/retrieveannotations/Main.java
@@ -0,0 +1,7 @@
+package com.example.reflection.intermediatelevel.retrieveannotations;
+public class Main {
+
+    public static void main(String[] args) {
+        AnnotationRetriever.retrieveAuthor(Book.class);
+    }
+}
